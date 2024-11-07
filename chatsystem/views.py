7bc2d27d97c5058c3d0d59f55e420859d1f1f954
Aswini Kumar <aswini0509@gmail.com>
2024-11-07 16:15:52 +0530

    Update textsplitter

--- chatsystem/views.py
+++ chatsystem/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import PDFUploadForm
 from .models import PDFUpload
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 # Create your views here.
 
 def upload_pdf(request):
@@ -12,12 +12,14 @@ def upload_pdf(request):
         if form.is_valid():
             pdf_instance = PyPDFLoader(form)
             loader = pdf_instance.load()
-            loader.save()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            texts = text_splitter.split_documents(loader)
+            texts.save()
             return redirect('upload_success')
     else:
         form = PDFUploadForm()
-    return render(request, 'upload_pdf.html', {'form': form})
+    return render(request, 'chatsystem/upload_pdf.html', {'form': form})
 
 
 def upload_success(request):
-    return render(request, 'upload_success.html')
+    return render(request, 'chatsystem/upload_success.html')
